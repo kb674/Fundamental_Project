61d5e72e921b723d8016bc7b2206d43c0ea6c338
kb674 <kushalbhikhabhai@gmail.com>
2021-05-11 11:13:06 +0000

    Boarders and Tricks have crud seperate CRUD functionality

--- application/models.py
+++ application/models.py
@@ -7,10 +7,14 @@ class Task_table(db.Model):
     description = db.Column(db.String(50), nullable = False)
     task_status = db.Column(db.Boolean, nullable = False, default = False)
     date_created = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
+   
+class Boarders(db.Model):
+    boarder_id = db.Column(db.Integer, primary_key = True)
+    boarder_name = db.Column(db.String(20), nullable = False)
+    tricks = db.relationship('Tricks', backref='boarder')
 
 class Tricks(db.Model):
     trick_id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(20), nullable = False)
-    longboard = db.Column(db.Boolean, nullable = False, default = False)
+    trick_name = db.Column(db.String(15), nullable = False)
+    fk_boarder_id = db.Column(db.Integer, db.ForeignKey('boarders.boarder_id'), nullable = True)
 
-    
--- application/routes.py
+++ application/routes.py
@@ -1,48 +1,64 @@
 from application import app, db
-from application.models import Task_table, Tricks
+from application.models import Task_table, Boarders, Tricks
 
 @app.route('/')
 
 @app.route('/home')
 def home():
-    all_tricks = Tricks.query.all()
+    all_boarders = Boarders.query.all()
     output = ""
-    for each_trick in all_tricks:
-        output += str(each_trick.trick_id) + "/   / "+ each_trick.name + " - " + str(each_trick.longboard) + "<br>"
+    for each_boarder in all_boarders:
+        output += str(each_boarder.boarder_id) + " |    "+ each_boarder.boarder_name + "<br>"
     return output
 
 @app.route('/create')
 def create():
-    new_trick = Tricks(name = 'cross step')
+    new_boarder = Boarders(boarder_name = 'skate_master_5000')
+    db.session.add(new_boarder)
+    db.session.commit()
+    return f"A new boarder has been added"
+
+@app.route('/update/<new_name>')
+def update(new_name):
+    boarder_name_change = Boarders.query.order_by(Boarders.boarder_id.desc()).first()
+    boarder_name_change.boarder_name = new_name
+    db.session.commit()
+    return f"You have changed the name of the boarder to {new_name}"
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    boarder_delete = Boarders.query.filter_by(boarder_id=id).first()
+    db.session.delete(boarder_delete)
+    db.session.commit()
+    return f"Boarder {id} has been deleted"
+
+
+@app.route('/trick_home')
+def trick_home():
+    all_tricks = Tricks.query.all()
+    output = ""
+    for each_trick in all_tricks:
+        output += str(each_trick.trick_id) + " |    "+ each_trick.trick_name + " - " + str(each_trick.fk_boarder_id) + "<br>"
+    return output
+
+
+@app.route('/trick_create')
+def trick_create():
+    new_trick = Tricks(trick_name = 'shuvit')
     db.session.add(new_trick)
     db.session.commit()
     return f"A new trick has been added"
 
-
-@app.route('/longboard/<int:id>')
-def longboard(id):
-    trick_change = Tricks.query.filter_by(trick_id = id).first()
-    trick_change.longboard = True
+@app.route('/trick_update/<new_name>')
+def trick_update(new_name):
+    trick_name_change = Tricks.query.order_by(Tricks.trick_id.desc()).first()
+    trick_name_change.trick_name = new_name
     db.session.commit()
-    return f"Trick {id} has been categorised as a longboard trick!"
+    return f"You have changed the name of the most rescent trick to {new_name}"
 
-@app.route('/not_longboard/<int:id>')
-def not_longboard(id):
-    trick_to_change = Tricks.query.filter_by(trick_id = id).first()
-    trick_to_change.longboard = False
-    db.session.commit()
-    return f"Trick {id} has been removed from the longboard list."
-
-@app.route('/update/<new_name>')
-def update(new_name):
-    trick_change = Tricks.query.order_by(Tricks.trick_id.desc()).first()
-    trick_change.name = new_name
-    db.session.commit()
-    return f"You have changed the name of the most recent trick"
-
-@app.route('/delete/<int:id>')
-def delete(id):
-    trick_to_delete = Tricks.query.filter_by(trick_id=id).first()
-    db.session.delete(trick_to_delete)
+@app.route('/trick_delete/<int:id>')
+def trick_delete(id):
+    trick_delete = Tricks.query.filter_by(trick_id=id).first()
+    db.session.delete(trick_delete)
     db.session.commit()
     return f"Trick {id} has been deleted"
