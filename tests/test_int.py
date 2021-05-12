94b42bf0f8b7848f5a7d72af5a14dff2707c292d
kb674 <kushalbhikhabhai@gmail.com>
2021-05-12 15:22:12 +0000

    Written a total of 4 integration tests, have 4 passes!

--- tests/test_int.py
+++ tests/test_int.py
@@ -39,23 +39,36 @@ class TestBase(LiveServerTestCase):
         self.assertEqual(response.code, 200)
 
 class TestAdd(TestBase):
-
     def add_boarder_button(self):
         button = self.driver.find_element_by_xpath('/html/body/a[2]')
         button.click()
         self.assertEqual(urlfor("create"))
 
-    def add_boarder_button(self):
+class TestAdd_trick(TestBase):
+    def add_boarder_trick(self):
         button = self.driver.find_element_by_xpath('/html/body/a[3]')
         button.click()
         self.assertEqual(urlfor("trick_create"))
-        
+
+class TestAdd_board_enter_name(TestBase):
     def add_boarder_enter_name(self):
         button = self.driver.find_element_by_xpath('/html/body/a[2]')
         button.click()
         field = self.driver.find_element_by_xpath('//*[@id="boarder_name"]')
         field = field.send_keys("skate_master_2121")
         self.assertIn(url_for('home'), field)
+
+class TestAdd_trick_enter_name(TestBase):
+    def add_trick_enter_name(self):
+        button = self.driver.find_element_by_xpath('/html/body/a[2]')
+        button.click()
+        field1 = self.driver.find_element_by_xpath('//*[@id="trick_name"]')
+        field1 = field1.send_keys("skate_master_2121")
+        field2 = self.driver.find_element_by_xpath('//*[@id="fk_boarder_id"]')
+        field2 = field2.send_keys('1')
+        self.assertIn(url_for('home'), field1)
+        self.assertIn(url_for('home'), field2)
+ 
 
 
 
